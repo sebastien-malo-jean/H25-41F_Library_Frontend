3abbe78d5538a3ece882710d131ab71650a15a5b
Sebastien Malo Jean <e2296673@cmaisonneuve.qc.ca>
2025-03-06 20:21:01 -0500

    ajouter le composant CharacterDetail__right, restructurer le composant CharacterDetails et améliorer le style des détails du personnage

---
+++ src/components/CharacterDetails/CharacterDetail__right/CharacterDetail__right.jsx
@@ -0,0 +1,102 @@
+//css
+import "./CharacterDetail__right.css";
+//bibliothèques
+function CharacterDetail__right() {
+  return (
+    <section className="characterDetail__right">
+      <section className="CharacterBasicInfo">
+        <h3>Information de base</h3>
+        <p>
+          <strong>Vocation :</strong> {character.charVoc}
+        </p>
+        <p>
+          <strong>Nom :</strong> {character.name}
+        </p>
+        <p>
+          <strong>Genre :</strong> {character.genre}
+        </p>
+        <p>
+          <strong>Classe :</strong> {character.class}
+        </p>
+        <p>
+          <strong>Race :</strong> {character.race}
+        </p>
+        <p>
+          <strong>Alignement :</strong> {character.alignement[0]}-
+          {character.alignement[1]}
+        </p>
+        <p>
+          <strong>Expérience :</strong> {character.exp}
+        </p>
+        <p>
+          <strong>Niveau :</strong> {character.lvl}
+        </p>
+      </section>
+      <section className="CharacterTraits">
+        <h3>Traits :</h3>
+        <ul>
+          <li>
+            <p>
+              <strong>Traits de personalitée :</strong>{" "}
+              {character.Traits.PersonalityTraits}
+            </p>
+          </li>
+          <li>
+            <p>
+              <strong>Idéaux :</strong> {character.Traits.Ideals}
+            </p>
+          </li>
+          <li>
+            <p>
+              <strong>attachement profond :</strong> {character.Traits.Bonds}
+            </p>
+          </li>
+          <li>
+            <p>
+              <strong>Défauts :</strong> {character.Traits.Flaws}
+            </p>
+          </li>
+        </ul>
+      </section>
+      <section className="CharacterStatistics">
+        <h3>statistics</h3>
+        <ul>
+          <li>
+            <p>
+              <strong>Force :</strong> {character.statistics.strength}
+            </p>
+          </li>
+          <li>
+            <p>
+              <strong>Dextérité :</strong> {character.statistics.dexterity}
+            </p>
+          </li>
+          <li>
+            <p>
+              <strong>Constitution :</strong>{" "}
+              {character.statistics.constitution}
+            </p>
+          </li>
+          <li>
+            <p>
+              <strong>Intelligence :</strong>{" "}
+              {character.statistics.intelligence}
+            </p>
+          </li>
+          <li>
+            <p>
+              <strong>Sagesse :</strong> {character.statistics.wisdom}
+            </p>
+          </li>
+          <li>
+            <p>
+              <strong>Charisme :</strong> {character.statistics.charisma}
+            </p>
+          </li>
+        </ul>
+      </section>
+    </section>
+  );
+}
+
+export default CharacterDetail__right;
--- src/components/CharacterDetails/CharacterDetails.jsx
+++ src/components/CharacterDetails/CharacterDetails.jsx
@@ -1,6 +1,10 @@
+//css
 import "./CharacterDetails.css";
+//bibliotheque
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom"; // Import pour récupérer l'ID
+//composants
+import CharacterDetail__right from "./characterDetail__right/CharacterDetail__right";
 
 function CharacterDetails() {
   const { id } = useParams(); // Récupère l'ID depuis l'URL
@@ -34,111 +38,27 @@ function CharacterDetails() {
     <main>
       <h2>Détail du personnage</h2>
       <section className="characterDetail">
-        <picture className="characterPicture">
-          <img alt={character.name} />
-        </picture>
-        <section className="CharacterBasicInfo">
-          <h3>Information de base</h3>
-          <p>
-            <strong>Vocation :</strong> {character.charVoc}
-          </p>
-          <p>
-            <strong>Nom :</strong> {character.name}
-          </p>
-          <p>
-            <strong>Genre :</strong> {character.genre}
-          </p>
-          <p>
-            <strong>Classe :</strong> {character.class}
-          </p>
-          <p>
-            <strong>Race :</strong> {character.race}
-          </p>
-          <p>
-            <strong>Alignement :</strong> {character.alignement[0]}-
-            {character.alignement[1]}
-          </p>
-          <p>
-            <strong>Expérience :</strong> {character.exp}
-          </p>
-          <p>
-            <strong>Niveau :</strong> {character.lvl}
-          </p>
+        <section className="characterDetail__title">
+          <h3>{character.name}</h3>
         </section>
-        <section className="CharacterTraits">
-          <h3>Traits :</h3>
-          <ul>
-            <li>
-              <p>
-                <strong>Traits de personalitée :</strong>{" "}
-                {character.Traits.PersonalityTraits}
-              </p>
-            </li>
-            <li>
-              <p>
-                <strong>Idéaux :</strong> {character.Traits.Ideals}
-              </p>
-            </li>
-            <li>
-              <p>
-                <strong>attachement profond :</strong> {character.Traits.Bonds}
-              </p>
-            </li>
-            <li>
-              <p>
-                <strong>Défauts :</strong> {character.Traits.Flaws}
-              </p>
-            </li>
-          </ul>
+        <section className="characterDetail__left">
+          <picture className="characterPicture">
+            <img alt={character.name} />
+          </picture>
+          <section className="CharacterHitpoints">
+            <h3>Point de vie</h3>
+            <p>
+              <strong>Point de vie total :</strong>{" "}
+              {character.hitPoints.totalHP}
+            </p>
+            <p>
+              <strong>Point de vie acutel :</strong>{" "}
+              {character.hitPoints.currentHP}
+            </p>
+          </section>
         </section>
-        <section className="CharacterHitpoints">
-          <h3>Point de vie</h3>
-          <p>
-            <strong>Point de vie total :</strong> {character.hitPoints.totalHP}
-          </p>
-          <p>
-            <strong>Point de vie acutel :</strong>{" "}
-            {character.hitPoints.currentHP}
-          </p>
-        </section>
-        <section className="CharacterStatistics">
-          <h3>statistics</h3>
-          <ul>
-            <li>
-              <p>
-                <strong>Force :</strong> {character.statistics.strength}
-              </p>
-            </li>
-            <li>
-              <p>
-                <strong>Dextérité :</strong> {character.statistics.dexterity}
-              </p>
-            </li>
-            <li>
-              <p>
-                <strong>Constitution :</strong>{" "}
-                {character.statistics.constitution}
-              </p>
-            </li>
-            <li>
-              <p>
-                <strong>Intelligence :</strong>{" "}
-                {character.statistics.intelligence}
-              </p>
-            </li>
-            <li>
-              <p>
-                <strong>Sagesse :</strong> {character.statistics.wisdom}
-              </p>
-            </li>
-            <li>
-              <p>
-                <strong>Charisme :</strong> {character.statistics.charisma}
-              </p>
-            </li>
-          </ul>
-        </section>
-        <section className="characterDescription">
+        <CharacterDetail__right key={character.id} character={character} />
+        <section className="characterDetail__description">
           <h3>Description</h3>
           <p>
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorum
