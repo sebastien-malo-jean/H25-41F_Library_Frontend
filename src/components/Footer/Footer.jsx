f029fce9aa2621aeaf6eafb0767602e6f9c67de3
Sebastien Malo Jean <e2296673@cmaisonneuve.qc.ca>
2025-03-17 08:19:28 -0400

    refactorisation : mise à jour du style et de la structure du composant Footer pour une meilleure lisibilité et réactivité

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,12 @@
 import "./Footer.css";
+
 function Footer() {
   const year = new Date().getFullYear();
 
   return (
-    <footer>
-      <h1>Créateur de personnage</h1>
-      <p>Tous droits réservés - {year}</p>
+    <footer className="footer text-center p-3">
+      <h1 className="footer__title">Créateur de personnage</h1>
+      <p className="footer__text">Tous droits réservés - {year}</p>
     </footer>
   );
 }
