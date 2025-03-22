72e9b7a0742bc09304d6bba9e5d6005fd2221107
Sebastien Malo Jean <e2296673@cmaisonneuve.qc.ca>
2025-03-22 19:29:52 -0400

    refactorisation : amélioration du style et de la structure du composant Home, ajout d'un bouton pour naviguer vers la liste des personnages

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,26 @@
 import React from "react";
 import "./Home.css";
+import { useNavigate } from "react-router-dom";
 
 function Home() {
+  const navigate = useNavigate();
+
   return (
-    <main>
-      <h2>Accueil</h2>
+    <main className="home-container">
+      <section className="hero">
+        <h1>Bienvenue dans l'univers des Personnages</h1>
+        <p>
+          Découvrez une vaste collection de personnages issus de différents
+          mondes, classes et races. Filtrez, explorez et trouvez votre héros
+          préféré !
+        </p>
+        <button
+          className="btn btn-primary"
+          onClick={() => navigate("/characters")}
+        >
+          Explorer les Personnages
+        </button>
+      </section>
     </main>
   );
 }
