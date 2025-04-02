2748f7d81877c6165e64ae96e0674c81d00c9a19
Sebastien Malo Jean <e2296673@cmaisonneuve.qc.ca>
2025-04-01 23:23:24 -0400

    refactorisation : ajout d'un formulaire de connexion dans le composant Header avec gestion des erreurs et état de chargement

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,48 @@
 import "./Header.css";
 import { NavLink } from "react-router-dom";
+import { useRef, useState } from "react";
 import logo from "../../assets/img/logo.jpg";
 
 function Header() {
+  const formRef = useRef();
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  async function sendForm(event) {
+    //bloquer l'envoi
+    event.preventDefault();
+    //récupération du body
+    const { email, password } = formRef.current;
+    const body = { email: email.value, password: password.value };
+
+    const options = {
+      body: JSON.stringify(body),
+      headers: {
+        "Content-type": "application/json",
+      },
+      method: "POST",
+    };
+    let URL = "https://h25-41f-library.onrender.com/users/connection";
+    setIsLoading(true);
+    setError(null);
+    //validation des champs
+    //envoyer
+    try {
+      const response = await fetch(URL, options);
+      if (!response.ok) {
+        //sinon on affiche une erreur
+        throw new error("la connexion a échoué.");
+      }
+      //Si la connexion est bonne, on sauvegarde l'info
+      const datas = await response.json();
+      console.log(datas);
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
   return (
     <header className="header">
       <picture className="header__logo">
@@ -32,7 +72,29 @@ function Header() {
           </li>
         </ul>
       </nav>
-      <h1 className="header__title">Créateur de personnage</h1>
+      <form
+        ref={formRef}
+        className="header__connectionForm"
+        onSubmit={sendForm}
+      >
+        <h1 className="header__title">Créateur de personnage</h1>
+        <div className="input-group">
+          <label htmlFor="email">Courriel</label>
+          <input type="email" name="email" id="email" required />
+        </div>
+        <div className="input-group">
+          <label htmlFor="password">Mot de passe</label>
+          <input type="password" name="password" id="password" required />
+        </div>
+        <input
+          type="submit"
+          value={isLoading ? "Chargement..." : "Connexion"}
+          disabled={isLoading}
+        />
+        {error && <p className="error-message">{error}</p>}
+        {/* affiche les messages d'érreurs */}
+      </form>
+      <div className="header__button">Déconnexion</div>
     </header>
   );
 }
